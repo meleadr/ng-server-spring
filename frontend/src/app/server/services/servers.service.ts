f8f138f062f0bbb408dc4a601bcb0638fb8c96c8
meleadr <meledina.adriano@gmail.com>
2023-08-30 13:59:35 +0200

    feat: servers service

--- frontend/src/app/server/services/servers.service.ts
+++ frontend/src/app/server/services/servers.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, delay, map, Observable, tap} from "rxjs";
+import {Server} from "../models/server.model";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ServersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private _loading$ = new BehaviorSubject<boolean>(false);
+  get loading$(): Observable<boolean> {
+    return this._loading$.asObservable();
+  }
+
+  private _servers$ = new BehaviorSubject<Server[]>([]);
+  get servers$(): Observable<Server[]> {
+    return this._servers$.asObservable();
+  }
+
+  private lastServerLoaded = 0;
+
+  private setLoadingStatus(status: boolean) {
+    this._loading$.next(status);
+  }
+
+  getServersFromServer() {
+    if(Date.now() - this.lastServerLoaded <= 3000000) {
+      return;
+    }
+    this.setLoadingStatus(true);
+    this.http.get<Server[]>('http://localhost:9000/server').pipe(
+      delay(1000),
+      tap(servers => {
+        this.lastServerLoaded = Date.now();
+        this._servers$.next(servers);
+        this.setLoadingStatus(false);
+      })
+    ).subscribe();
+  }
+
+  getServerById(id: number): Observable<Server> {
+    if(!this.lastServerLoaded){
+      this.getServersFromServer();
+    }
+    return this.servers$.pipe(
+      map(servers => servers.filter(server => server.id === id)[0])
+    )
+  }
 }
