bfe0842051fe42bb52261484f55ab8256ee86288
meleadr <meledina.adriano@gmail.com>
2023-08-30 13:57:33 +0200

    build: Material Module

--- frontend/src/app/shared/material.module.ts
+++ frontend/src/app/shared/material.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 
 
 @NgModule({
   exports: [
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
     ]
 })
 export class MaterialModule { }
