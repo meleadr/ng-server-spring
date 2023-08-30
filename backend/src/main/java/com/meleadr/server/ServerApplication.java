5b51ff7ef3346514ab106ca1783db26402a7a517
meleadr <meledina.adriano@gmail.com>
2023-08-30 11:47:48 +0200

    build: App
    Ajout du bean pour ajouter des servers

--- backend/src/main/java/com/meleadr/server/ServerApplication.java
+++ backend/src/main/java/com/meleadr/server/ServerApplication.java
@@ -1,7 +1,12 @@
 package com.meleadr.server;
 
+import com.meleadr.server.enumeration.Status;
+import com.meleadr.server.model.Server;
+import com.meleadr.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +15,14 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+
+//	@Bean
+//	CommandLineRunner runner(ServerRepository serverRepo) {
+//		return args -> {
+//			serverRepo.save(new Server(null, "192.168.0.1", "Server 1", "16GB", "Dedicated", "https://www.freepnglogos.com/uploads/server-png/server-icon-download-icons-17.png", Status.SERVER_UP));
+//			serverRepo.save(new Server(null, "192.168.0.2", "Server 2", "8GB", "App Web", "https://www.freepnglogos.com/uploads/server-png/server-icon-download-icons-17.png", Status.SERVER_UP));
+//			serverRepo.save(new Server(null, "192.168.0.3", "Server 3", "4GB", "Website", "https://www.freepnglogos.com/uploads/server-png/server-icon-download-icons-17.png", Status.SERVER_UP));
+//			serverRepo.save(new Server(null, "192.168.0.4", "Server 4", "64GB", "Mutu", "https://www.freepnglogos.com/uploads/server-png/server-icon-download-icons-17.png", Status.SERVER_UP));
+//		};
+//	}
 }
