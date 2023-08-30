5a410f42a4aa31ef2433884e87bf8cd993b4df21
meleadr <meledina.adriano@gmail.com>
2023-08-30 13:57:53 +0200

    fix: routing

--- frontend/src/app/server/server-routing.module.ts
+++ frontend/src/app/server/server-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {ServerListComponent} from "./components/server-list/server-list.component";
 
 const routes: Routes = [
-  { path: 'server', component: ServerListComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'server' },
+  { path: '', component: ServerListComponent },
 ];
 
 @NgModule({
