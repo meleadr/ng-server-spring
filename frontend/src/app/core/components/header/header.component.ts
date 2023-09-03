5b8c5f527ebb91459d30024f6aaf629ade5e597b
meleadr <meledina.adriano@gmail.com>
2023-09-03 19:37:06 +0200

    feat: refonte de la toolbar

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, ActivatedRouteSnapshot } from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ import { ActivatedRoute, NavigationEnd, Router, ActivatedRouteSnapshot } from "@
 export class HeaderComponent implements OnInit{
   subTitle!: string;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, public authService: AuthService) {
   }
 
   ngOnInit() {
