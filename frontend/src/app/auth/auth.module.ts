0b487dc01fe539c19ecb73edcf248b416b39cd6e
meleadr <meledina.adriano@gmail.com>
2023-09-03 19:36:48 +0200

    fix: Module auth
    Retrait du lazy loading du module auth

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import {SharedModule} from "../shared/shared.module";
 import {ReactiveFormsModule} from "@angular/forms";
@@ -13,7 +12,6 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule,
     SharedModule,
     ReactiveFormsModule
   ]
